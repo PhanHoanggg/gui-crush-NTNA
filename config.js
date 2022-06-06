5c759c3a1fb74ceb128cfe067e15dd9a9be30d4b
PhanHoanggg <89576327+PhanHoanggg@users.noreply.github.com>
2022-06-06 07:24:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,20 @@
-const CONFIG = {Gửi Ánh}
-    introTitle: 'Ánh ơi!',
-    introDesc: `Ánh à :3`,
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Mình...',
-    desc: 'Mình thích nụ cười của cậu ^^, cậu có thể cười cho mình nhìn mãi được không? ',
-    btnYes: 'Được nha :3',
-    btnNo: 'Còn lâu :(',
-    question:'Tớ thích cậu, gửi cho tớ tâm tư của cậu nhé :3,
-    btnReply: 'Gửi cho Hoàng <3',
-    reply: 'Tớ cũng thích cậu :3',
-    mess: 'Tớ thích cậu nhiều hơn ^^',
-    messDesc: 'Hôm nào rảnh cho phép tớ dẫn Ánh đi ăn gì ngon ngon nhá :3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
+```
